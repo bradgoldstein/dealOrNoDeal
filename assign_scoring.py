ac5374068928e629a28afda0badd8e8e92c895f0
BradleyGoldstein <bradgold@umich.edu>
2015-08-07 10:08:33 +0300

    working ?!

---
+++ assign_scoring.py
@@ -0,0 +1,96 @@
+from player import Player
+from card import Card
+from compare import compare
+
+#given custom ranking schemes as dictionaries, assign_scoring_schemes
+def assign_scoring (customRankingScheme, customCardCountScheme):
+    print customRankingScheme
+    print "dfdsf"
+
+    rankingScheme=dict()
+
+    def defaultRankingScheme():
+
+        return {
+            Card("hearts", 1): 0,
+            Card("hearts", 2): 0,
+            Card("hearts", 3): 0,
+            Card("hearts", 4): 0,
+            Card("hearts", 5): 0,
+            Card("hearts", 6): 0,
+            Card("hearts", 7): 0,
+            Card("hearts", 8): 0,
+            Card("hearts", 9): 0,
+            Card("hearts", 10): 0,
+            Card("hearts", 11): 0,
+            Card("hearts", 12): 0,
+            Card("hearts", 13): 0,
+
+            Card("diamonds", 1): 0,
+            Card("diamonds", 2): 0,
+            Card("diamonds", 3): 0,
+            Card("diamonds", 4): 0,
+            Card("diamonds", 5): 0,
+            Card("diamonds", 6): 0,
+            Card("diamonds", 7): 0,
+            Card("diamonds", 8): 0,
+            Card("diamonds", 9): 0,
+            Card("diamonds", 10): 0,
+            Card("diamonds", 11): 0,
+            Card("diamonds", 12): 0,
+            Card("diamonds", 13): 0,
+
+            Card("spades", 1): 0,
+            Card("spades", 2): 0,
+            Card("spades", 3): 0,
+            Card("spades", 4): 0,
+            Card("spades", 5): 0,
+            Card("spades", 6): 0,
+            Card("spades", 7): 0,
+            Card("spades", 8): 0,
+            Card("spades", 9): 0,
+            Card("spades", 10): 0,
+            Card("spades", 11): 0,
+            Card("spades", 12): 0,
+            Card("spades", 13): 0,
+
+            Card("clubs", 1): 0,
+            Card("clubs", 2): 0,
+            Card("clubs", 3): 0,
+            Card("clubs", 4): 0,
+            Card("clubs", 5): 0,
+            Card("clubs", 6): 0,
+            Card("clubs", 7): 0,
+            Card("clubs", 8): 0,
+            Card("clubs", 9): 0,
+            Card("clubs", 10): 0,
+            Card("clubs", 11): 0,
+            Card("clubs", 12): 0,
+            Card("clubs", 13): 0
+        }
+
+    rankingScheme=defaultRankingScheme()
+
+    for card in customRankingScheme:
+        rankingScheme[card]=customRankingScheme[card]
+        print rankingScheme[card]
+
+    def defaultCardCountScheme():
+
+        return { 'total':0, 'hearts':0, 'spades':0, 'diamonds':0, 'clubs':0  }
+
+    cardCountScheme=defaultCardCountScheme()
+
+    for suit in customCardCountScheme:
+        cardCountScheme[suit]=customCardCountScheme[suit]
+        print cardCountScheme[suit]
+
+#customRankingScheme={
+#    Card("clubs", 12): 10,
+#    Card("clubs", 13): 4
+#}
+
+#customCardCountScheme={
+#    'total':10, 'hearts':40
+#}
+#assign_scoring (customRankingScheme, customCardCountScheme)
--- game.py
+++ game.py
@@ -5,10 +5,9 @@ import json
 from card import parse_cards
 from player import Player
 from board import Board
+from action_handler import action_handler
 # from pile import Pile
-#read in file, parse,
 
-#play
 
 class Game(object):
     "an entire game is stored and played here"
@@ -28,40 +27,42 @@ class Game(object):
 
         for i, pile_json in enumerate(piles_json):
             cards_in_pile = parse_cards(pile_json['cards'])
-            owner_id = pile_json['ownerId']
+            owner_id = pile_json['owner_id']
             setting = pile_json['setting']
 
-            pile = Pile(i, cards_in_pile)
-            piles[i] = pile
+# TODO waiting on shaul
+            # pile = Pile(i, cards_in_pile)
+            # piles[i] = pile
+            #
+            # # not owned by anyone
+            # if owner_id == -1:
+            #     assert setting in [2,3]
+            #     if setting == 2: # public pile
+            #         self.board.pub.append(pile)
+            #     else: # secret pile
+            #         self.board.sec.append(pile)
+            # # owned by a specific Player
+            # else:
+            #     assert setting in [0,1,2,3]
+            #     if setting == 0:
+            #         players[owner_id].private.append(pile)
+            #     elif setting == 1:
+            #         players[owner_id].non_private.append(pile)
+            #     elif setting == 2:
+            #         players[owner_id].public.append(pile)
+            #     else: # secret
+            #         players[owner_id].secret.append(pile)
 
-            # not owned by anyone
-            if owner_id == -1:
-                assert setting in [2,3]
-                if setting == 2: # public pile
-                    self.board.pub.append(pile)
-                else: # secret pile
-                    self.board.sec.append(pile)
-            # owned by a specific Player
-            else:
-                assert setting in [0,1,2,3]
-                if setting == 0:
-                    players[owner_id].private.append(pile)
-                elif setting == 1:
-                    players[owner_id].non_private.append(pile)
-                elif setting == 2:
-                    players[owner_id].public.append(pile)
-                else: # secret
-                    players[owner_id].secret.append(pile)
+        self.play(nodes[0], nodes)
 
-    def play(self):
-        fsm_input = READ_FROM_FILE - fsm_input
-
-
-        while (this.game_is_not_over()):
-            self.play_round()
-        self.game_over()
-
-    def play_round(self):
-        pass
+    def play(self, node, nodes):
+        if node["isEnd"] is True:
+            print "End of game :) Thanks for playing!"
+        else:
+            action_handler(self, node['action'])
+            if_statements = node['conditions']
+            for state in if_statements:
+                if eval(state['boolFunc']):
+                    self.play(nodes[state['nodeId']], nodes)
 
 game = Game()
