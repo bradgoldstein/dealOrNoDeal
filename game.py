6e19b5a9ce5fff5b1c444be9ea91cf058c2e8322
BradleyGoldstein <bradgold@umich.edu>
2015-08-07 08:45:50 +0300

    good morning friday :)

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 __author__ = 'bradleygoldstein'
 
+
+import json
+from player import Player
 #read in file, parse,
 
 #play
@@ -9,18 +12,35 @@ class Game(object):
     def __init__(self, title):
         self.title = title
 
-        # pseudocode
-        players = get_players()
-        cards = get_cards()
-        ...
-        self.board = Board(players, cards, rules,...)
-        self.rules = ...
+        game_input = READ_FROM_FILE - game
+        piles = game_input['piles']
+        players = game_input['players']
+
+        for id in players:
+            players.append(Player(id))
+
+        for pile in piles:
+            cards_in_pile = pile['cards']
+    ## get the proper names for this...
+            id = pile['pileId']
+            owner_id = pile['ownerId']
+            #find owner of function
+
+        self.play()
 
 
     def play(self):
+        fsm_input = READ_FROM_FILE - fsm_input
+        
+
         while (this.game_is_not_over()):
-            this.play_round()
-        this.game_over()
+            self.play_round()
+        self.game_over()
 
     def play_round(self):
         pass
+
+
+    class Game
+        Pile piles
+        Player players
--- pile.py
+++ pile.py
@@ -4,7 +4,7 @@ Created on Aug 6, 2015
 @author: S Vin
 '''
 
-
+from random import shuffle
 
 class Pile(object):
     '''
@@ -19,3 +19,12 @@ class Pile(object):
         Pile.number += 1
         self.cards = []
 
+        
+
+    def transfer_stack(self, pile):                                         #transfer 1: moves first item of self to the top of pile
+        pile.append(self.pop([-1]))
+    def transfer_stack_multiple(self, pile, num):                           #transfer multiple items from the top of a pile to the top of another pile?
+        for _ in range(num): self.transfer_stack(pile)
+    def transfer_stack_spec(self, pile, pos):
+        pile.insert(self.pop[-1], pos)
+    def transfer_stack_multiple_spec(self, pile, range, pos):
